cb264e2eb73119954e2729e2d35c5e3ba55caaa1
LawKOMPRO <149090120+LawKOMPRO@users.noreply.github.com>
2024-11-15 15:33:27 +0800

    Update statics_manager.c
    行星搜索与检索初步实现

--- src/statics_manager.c
+++ src/statics_manager.c
@@ -1,3 +1,119 @@
-//
-// Created by 13206 on 24-11-4.
-//
+#include <stdio.h>
+#include <string.h>
+#include <tgmath.h>
+#include "statics_manager.h"
+
+// 按名称搜索行星
+void searchPlanetByName(Planet planets[], int numPlanets, const char* name) {
+    int found = 0;
+    for (int i = 0; i < numPlanets; i++) {
+        if (strcmp(planets[i].name, name) == 0) {
+            printf("Found Planet: %s\n", planets[i].name);
+            printf("Type: %s\n", planets[i].type);
+            printf("Size: %.2lf\n", planets[i].size);
+            printf("Distance from Star: %.2lf AU\n", planets[i].distanceFromStar);
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Planet not found.\n");
+    }
+}
+
+// 按类型搜索行星
+void searchPlanetType(Planet planets[], int numPlanets, const char* type) {
+    int found = 0;
+    for (int i = 0; i < numPlanets; i++) {
+        if (strcmp(planets[i].type, type) == 0) {
+            printf("Found Planet: %s\n", planets[i].name);
+            printf("Type: %s\n", planets[i].type);
+            printf("Size: %.2lf\n", planets[i].size);
+            printf("Distance from Star: %.2lf AU\n", planets[i].distanceFromStar);
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Planet not found.\n");
+    }
+}
+
+// 按尺寸搜索行星
+// 结果会输出最接近输入的行星
+void searchPlanetBySize(Planet planets[], int numPlanets, double size) {
+    double minDiff = __DBL_MAX__;  // 设置一个很大的初始差值
+    int closestIndex = -1;
+
+    for (int i = 0; i < numPlanets; i++) {
+        double diff = fabs(planets[i].size - size);  // 计算尺寸差的绝对值
+        if (diff < minDiff) {
+            minDiff = diff;
+            closestIndex = i;
+        }
+    }
+
+    if (closestIndex != -1) {
+        printf("Closest Planet by Size: %s\n", planets[closestIndex].name);
+        printf("Type: %s\n", planets[closestIndex].type);
+        printf("Size: %.2lf\n", planets[closestIndex].size);
+        printf("Distance from Star: %.2lf AU\n", planets[closestIndex].distanceFromStar);
+    } else {
+        printf("No planets found.\n");
+    }
+}
+
+// 按行星到恒星的距离搜索行星
+// 结果会输出最接近输入的行星
+void searchPlanetByDistance(Planet planets[], int numPlanets, double distance) {
+    double minDiff = __DBL_MAX__;  // 设置一个很大的初始差值
+    int closestIndex = -1;
+
+    for (int i = 0; i < numPlanets; i++) {
+        double diff = fabs(planets[i].distanceFromStar - distance);  // 计算距离差的绝对值
+        if (diff < minDiff) {
+            minDiff = diff;
+            closestIndex = i;
+        }
+    }
+
+    if (closestIndex != -1) {
+        printf("Closest Planet by Distance: %s\n", planets[closestIndex].name);
+        printf("Type: %s\n", planets[closestIndex].type);
+        printf("Size: %.2lf\n", planets[closestIndex].size);
+        printf("Distance from Star: %.2lf AU\n", planets[closestIndex].distanceFromStar);
+    } else {
+        printf("No planets found.\n");
+    }
+}
+
+// 显示所有行星信息
+void displayAllPlanets(Planet planets[], int numPlanets) {
+    for (int i = 0; i < numPlanets; i++) {
+        printf("Planet %d: %s\n", i + 1, planets[i].name);
+        printf("Type: %s, Size: %.2lf, Distance from Star: %.2lf AU\n", planets[i].type, planets[i].size, planets[i].distanceFromStar);
+    }
+}
+int main() {
+    Planet planets[5] = {
+        {"Earth", "Terrestrial", 12742.0, 1.0},
+        {"Mars", "Terrestrial", 6779.0, 1.52},
+        {"Jupiter", "Gas Giant", 139820.0, 5.2},
+        {"Saturn", "Gas Giant", 116460.0, 9.58},
+        {"Neptune", "Ice Giant", 49528.0, 30.07}
+    };
+
+    int numPlanets = 5;
+
+    // 搜索行星
+    char searchName[50];
+    printf("Enter the name of the planet to search: ");
+    scanf("%s", searchName);
+    searchPlanetByName(planets, numPlanets, searchName); //传入参数为 1.行星数组 2.数组的长度 3.要搜索行星的名称
+
+    // 显示所有行星信息
+    printf("\nDisplaying all planets:\n");
+    displayAllPlanets(planets, numPlanets);
+
+    return 0;
+}

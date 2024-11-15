b6d064047cd8e8f3fa819f0a0e34b72971d9dd4e
LawKOMPRO <149090120+LawKOMPRO@users.noreply.github.com>
2024-11-15 20:44:27 +0800

    Update statics_manager.h

--- include/statics_manager.h
+++ include/statics_manager.h
@@ -7,13 +7,16 @@ typedef struct {
     char type[50];
     double size;
     double distanceFromStar;
+    int isCataloged;   // 是否编入目录 (1 表示已编入, 0 表示未编入)
+    int isDiscovered;  // 是否被发现 (1 表示已发现, 0 表示未发现)
+    int isObserved;    // 是否被观测到 (1 表示已观测, 0 表示未观测)
 } Planet;
 
 // 传入参数为 1.行星数组 2.数组的长度 3.要搜索行星的名称
-void searchPlanetByName(Planet planets[], int numPlanets, const char* name);
-void searchPlanetType(Planet planets[], int numPlanets, const char* type);
-void searchPlanetBySize(Planet planets[], int numPlanets, double size);
-void searchPlanetByDistance(Planet planets[], int numPlanets, double distance);
-void displayAllPlanets(Planet planets[], int numPlanets);
+void searchPlanetByName(const char *filename, const char* name);
+void searchPlanetType(const char *filename, const char* type);
+void searchPlanetBySize(const char *filename, double size);
+void searchPlanetByDistance(const char *filename, double distance);
+void displayAllPlanets(const char *filename);
 
 #endif //STATICS_MANAGER_H

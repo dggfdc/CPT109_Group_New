9c7f0e6cb75b83b65575c19bcbce0f255d6c562c
qzrcarry <2758620972@qq.com>
2024-11-16 19:40:32 +0800

    1

--- include/researcher_manager.h
+++ include/researcher_manager.h
@@ -1,19 +1,18 @@
-#ifndef RESEARCHER_CATALOGUE_H
-#define RESEARCHER_CATALOGUE_H
+#ifndef RESEARCHER_MANAGER_H
+#define RESEARCHER_MANAGER_H
 
 #define MAX_NAME_LEN 50
 
-// 研究者信息结构体
 typedef struct {
     int account_number;      // 唯一账号
     char name[MAX_NAME_LEN]; // 研究者姓名
     int contributions;       // 贡献数量
 } Researcher;
 
-// 研究者管理函数
+// 函数声明
 void addResearcher(Researcher researcher);
 void editResearcher(int account_number, const char* new_name);
 void deleteResearcher(int account_number);
 void viewResearchers();
 
-#endif
+#endif // RESEARCHER_MANAGER_H

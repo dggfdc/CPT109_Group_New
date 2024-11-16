f50580420b57c21e4c18912080c21fd51c0a7e42
LawKOMPRO <149090120+LawKOMPRO@users.noreply.github.com>
2024-11-16 22:33:43 +0800

    Update statics_manager.h
    解决内存对齐导致的输出问题

--- include/statics_manager.h
+++ include/statics_manager.h
@@ -2,6 +2,7 @@
 #define STATICS_MANAGER_H
 
 // 行星结构体
+#pragma pack(1) // 禁用结构体对齐
 typedef struct {
     char name[50];
     char type[50];
@@ -11,6 +12,7 @@ typedef struct {
     int isDiscovered;  // 是否被发现 (1 表示已发现, 0 表示未发现)
     int isObserved;    // 是否被观测到 (1 表示已观测, 0 表示未观测)
 } Planet;
+#pragma pack() // 恢复默认对齐
 
 // 传入参数为 1.行星数组 2.数组的长度 3.要搜索行星的名称
 void searchPlanetByName(const char *filename, const char* name);

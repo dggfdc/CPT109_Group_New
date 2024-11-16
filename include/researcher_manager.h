20923f57a9c9665710d72216e814817657a4c820
LawKOMPRO <149090120+LawKOMPRO@users.noreply.github.com>
2024-11-16 23:38:33 +0800

    Update researcher_manager.h

---
+++ include/researcher_manager.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_MANAGER_H
+#define SEARCH_MANAGER_H
+
+// 定义结构体用于存储研究人员信息
+typedef struct {
+    int account_number;
+    char name[50];
+    char email[50];
+} Researcher;
+
+void register_researcher(int account_number, char* name, char* email);
+void edit_researcher_details(int account_number, char* new_name, char* new_email);
+void delete_researcher(int account_number);
+void view_researcher_contributions(int account_number);
+
+#endif //SEARCH_MANAGER_H

b422c707dbf63ef0a1d59ff6d982b2d5ea995d9c
dggfdc <2042016556@qq.com>
2024-12-04 20:06:52 +0800

    Update planet_manager.h

--- include/planet_manager.h
+++ include/planet_manager.h
@@ -1,4 +1,33 @@
 #ifndef PLANET_MANAGER_H
 #define PLANET_MANAGER_H
-void hello();
-#endif //PLANET_MANAGER_H
+
+
+// Constants
+#define MAX_PLANETS 100
+#define NAME_LENGTH 50
+#define TYPE_LENGTH 20
+#define SIZE_LENGTH 20
+#define RELEVANT_DATA_LENGTH 100
+#define RELEVANT_DATA_COUNT 5
+
+// Struct definition
+typedef struct {
+    char name[NAME_LENGTH];
+    char type[TYPE_LENGTH];
+    char size[SIZE_LENGTH];
+    double distanceFromStar; // In astronomical units (AU)
+    char relevantData[RELEVANT_DATA_COUNT][RELEVANT_DATA_LENGTH];
+} Planeta;
+
+// Global variables
+extern Planeta catalogue[MAX_PLANETS];
+extern int planetCount;
+
+// Function declarations
+void addPlanet();
+void editPlanet();
+void removePlanet();
+void displayCatalogue();
+void planet_manager();
+
+#endif // PLANET_MANAGER_H
